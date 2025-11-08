6df9c10d21da1206e3ea1e33fbb03abbdafbe6ce
Tobias Hägglund <tobhag@student.chalmers.se>
2025-11-08 15:55:53 +0100

    Task 4 complete, Smith-Waterman algorithm implemented and checked against online tool

---
+++ 1/local_alignment.c
@@ -0,0 +1,237 @@
+/*
+ * To compile this C program, placing the executable file in 'global', type:
+ *
+ *      gcc -o global global_alignment.c
+ *
+ * To run the program, type:
+ *
+ *      ./global
+ */
+
+#include <stdio.h>
+
+#define MAX_LENGTH	100
+
+#define MATCH_SCORE	2
+#define MISMATCH_SCORE	-1
+#define GAP_PENALTY	2
+
+#define STOP		0
+#define UP		1
+#define LEFT		2
+#define DIAG		3
+
+
+main()
+{ 
+	int	i, j;
+	int	m, n;
+	int	alignmentLength, score, tmp;
+	int maxScore = 0, maxI = 0, maxJ = 0;
+	char	X[MAX_LENGTH+1] = "PAWHEAE";
+	char	Y[MAX_LENGTH+1] = "HDAGAWGHEQ";
+
+	int	F[MAX_LENGTH+1][MAX_LENGTH+1];		/* score matrix */
+	int	trace[MAX_LENGTH+1][MAX_LENGTH+1];	/* trace matrix */
+	char	alignX[MAX_LENGTH*2];		/* aligned X sequence */
+	char	alignY[MAX_LENGTH*2];		/* aligned Y sequence */
+
+	/*
+	 * Find lengths of (null-terminated) strings X and Y
+	 */
+	m = 0;
+	n = 0;
+	while ( X[m] != 0 ) {
+		m++;
+	}
+	while ( Y[n] != 0 ) {
+		n++;
+	}
+
+
+	/*
+	 * Initialise matrices
+	 */
+
+	for ( i=0 ; i<=m ; i++ ) {
+		F[i][0] = 0;
+		trace[i][0] = STOP;
+	}
+	for ( j=0 ; j<=n ; j++ ) {
+		F[0][j] = 0;
+		trace[0][j] = STOP;
+	}
+
+
+ 	/*
+	 * Fill matrices
+	 */
+
+	for ( i=1 ; i<=m ; i++ ) {
+
+		for ( j=1 ; j<=n ; j++ ) {
+	
+			if ( X[i-1]==Y[j-1] ) {
+				score = F[i-1][j-1] + MATCH_SCORE;
+			} else {
+				score = F[i-1][j-1] + MISMATCH_SCORE;
+			}
+			trace[i][j] = DIAG;
+
+			tmp = F[i-1][j] - GAP_PENALTY;
+			if ( tmp>score ) {
+				score = tmp;
+				trace[i][j] = UP;
+			}
+
+			tmp = F[i][j-1] - GAP_PENALTY;
+			if( tmp>score ) {
+				score = tmp;
+				trace[i][j] = LEFT;
+			}
+
+			if ( score < 0 ) {
+				score = 0;
+				trace[i][j] = STOP;
+			}
+
+			F[i][j] = score;
+
+			if ( score > maxScore ) {
+				maxScore = score;
+				maxI = i;
+				maxJ = j;
+			}
+ 		}
+	}
+
+
+	/*
+	 * Print score matrix
+	 */
+
+	printf("Score matrix:\n      ");
+	for ( j=0 ; j<n ; ++j ) {
+		printf("%5c", Y[j]);
+	}
+	printf("\n");
+	for ( i=0 ; i<=m ; i++ ) {
+		if ( i==0 ) {
+			printf(" ");
+		} else {
+			printf("%c", X[i-1]);
+		}
+		for ( j=0 ; j<=n ; j++ ) {
+			printf("%5d", F[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+
+	/*
+	 * Trace back from the lower-right corner of the matrix
+	 */
+
+	i = maxI;
+	j = maxJ;
+	alignmentLength = 0;
+
+
+
+	while ( i > 0 && j > 0 && F[i][j] > 0 ) {
+
+		switch ( trace[i][j] ) {
+
+			case DIAG:
+				alignX[alignmentLength] = X[i-1];
+				alignY[alignmentLength] = Y[j-1];
+				i--;
+				j--;
+				alignmentLength++;
+				break;
+
+			case LEFT:
+				alignX[alignmentLength] = '-';
+				alignY[alignmentLength] = Y[j-1];
+				j--;
+				alignmentLength++;
+				break;
+
+			case UP:
+				alignX[alignmentLength] = X[i-1];
+				alignY[alignmentLength] = '-';
+				i--;
+				alignmentLength++;
+		}
+	}
+
+
+	/*
+	 * Unaligned beginning
+	 */
+
+	while ( i>0 ) {
+		alignX[alignmentLength] = X[i-1];
+		alignY[alignmentLength] = '-';
+		i--;
+		alignmentLength++;
+	}
+
+	while ( j>0 ) {
+		alignX[alignmentLength] = '-';
+		alignY[alignmentLength] = Y[j-1];
+		j--;
+		alignmentLength++;
+	}
+
+
+	/*
+	 * Print alignment
+	 */
+
+	for ( i=alignmentLength-1 ; i>=0 ; i-- ) {
+		printf("%c",alignX[i]);
+	}
+	// Task 1
+	printf("\n");
+	for ( i=alignmentLength-1 ; i>=0 ; i-- ) {
+		if ( alignX[i] == alignY[i] && alignX[i] != '-' ) {
+			printf("|");
+		}
+		else {
+			printf(" ");
+		}
+	}
+	printf("\n");
+	for ( i=alignmentLength-1 ; i>=0 ; i-- ) {
+		printf("%c",alignY[i]);
+	}
+	printf("\n");
+
+	/* Task 2
+	We only count positions where the two aligned characters are the same and neither side has a gap.
+	We divide this count by the total number of alignment columns. 
+	*/ 
+	int matches = 0;
+	for ( i = 0; i < alignmentLength; i++ ) {
+		if ( alignX[i] == alignY[i] && alignX[i] != '-' && alignY[i] != '-' ) {
+			matches++;
+		}
+	}
+
+	double percent_identity = ((double)matches / (double)alignmentLength) * 100.0;
+	printf("\nPercent Identity: %.2f%%\n", percent_identity);
+
+	// Task 3
+	int missmatches = 0;
+	for ( i = 0; i < alignmentLength; i++ ) {
+		if ( alignX[i] != alignY[i] ) {
+			missmatches++;
+		}
+	}
+
+	printf("Hamming Distance:%2d\n", missmatches);
+	
+	return(1);
+}
